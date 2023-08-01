b907a8522f827e24991c17f094b57cea48d50c77
DalerKhusainov <dalerkhusainov85@gmail.com>
2023-08-01 10:42:42 +0500

    Theer was an issue in the sidebar. The buttons view cart and checkout were outside of viewport. It was fixed.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -28,50 +28,50 @@ const Sidebar = () => {
     <div
       className={`${
         isOpen ? "right-0" : "-right-full"
-      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[45vw] xl:max-w-[35vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex items-center justify-between py-6 border-b">
         {/* AMOUNT PRODUCTS IN THE BAG */}
-        <div className="uppercase text-sm font-semibold">
+        <div className="text-sm font-semibold uppercase">
           Shopping Bag ({itemAmount})
         </div>
         {/* CLOSE ICON */}
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+          className="flex items-center justify-center w-8 h-8 cursor-pointer"
         >
           <IoMdArrowForward className="text-2xl" />
         </div>
       </div>
       {/* CART ITEMS */}
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
+      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[580px] overflow-y-auto overflow-x-hidden border-b">
         {cart?.map((product) => {
           return <CartItem product={product} key={product.id} />;
         })}
       </div>
-      <div className="flex flex-col gap-y-3 py-4 mt-4">
-        <div className="flex w-full justify-between items-center mb-4">
+      <div className="flex flex-col py-4 mt-4 gap-y-3">
+        <div className="flex items-center justify-between w-full mb-4">
           {/* TOTAL */}
-          <div className="uppercase font-semibold">
+          <div className="font-semibold uppercase">
             <span className="mr-2">Total:</span>$ {parseFloat(total).toFixed(2)}
           </div>
           {/* CLEAR CART ICON */}
           <div
             onClick={clearCart}
-            className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl"
+            className="flex items-center justify-center w-12 h-12 py-4 text-xl text-white bg-red-500 cursor-pointer"
           >
             <FiTrash2 />
           </div>
         </div>
         <Link
           to={"/"}
-          className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium"
+          className="flex items-center justify-center w-full p-4 font-medium bg-gray-200 text-primary"
         >
           View cart
         </Link>
         <Link
           to={"/"}
-          className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium"
+          className="flex items-center justify-center w-full p-4 font-medium text-white bg-primary"
         >
           Checkout
         </Link>
